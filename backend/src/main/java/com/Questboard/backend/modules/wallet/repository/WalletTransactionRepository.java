c21cc9f7a8e759cfe1980f9c981b032f1fc0622d
Karan Pareek <karanpareek1112@gmail.com>
2026-05-25 21:29:05 +0530

    added repository method in wallet transaction repository

--- backend/src/main/java/com/Questboard/backend/modules/wallet/repository/WalletTransactionRepository.java
+++ backend/src/main/java/com/Questboard/backend/modules/wallet/repository/WalletTransactionRepository.java
@@ -2,10 +2,13 @@ package com.Questboard.backend.modules.wallet.repository;
 
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.Questboard.backend.modules.wallet.models.WalletTransaction;
 
-public interface WalletTransactionRepository extends JpaRepository<WalletTransaction, UUID>{
-    
+public interface WalletTransactionRepository extends JpaRepository<WalletTransaction, UUID> {
+
+    Page<WalletTransaction> findByUserIdOrderByCreatedAtDesc(UUID userId, Pageable pageable);
 }
